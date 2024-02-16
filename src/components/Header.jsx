44c3e43a7ab239edf3724c754d8d458bcd58946f
JoaoVitorWinter <joaowinter@weg.net>
2024-02-16 20:42:21 -0300

    Adicionando botão de cadastrar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import { FaSearch } from "react-icons/fa";
 import Input from "./Input";
 
 const Header = () => {
--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button"
 import Header from "../components/Header"
 
 const InitialPage = () => {
@@ -5,6 +6,7 @@ const InitialPage = () => {
   return (
     <>
     <Header />
+    <Button onClick={() => {alert("Teste")}}text={"Cadastrar"} />
     </>
   )
 }
