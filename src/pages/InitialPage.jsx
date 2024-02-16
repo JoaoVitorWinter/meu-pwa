d9cc5c55d6620c908d52aee78b67b04883e246fe
MathiasDziurkowski <mathias_dziurkowski@estudante.sesisenai.org.br>
2024-02-16 20:23:15 -0300

    Adicionando header e estilizando-o

---
+++ src/pages/InitialPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+
+const InitialPage = () => {
+
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default InitialPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green": "#B0F2B4"
+      }
+    },
   },
   plugins: [],
 }
