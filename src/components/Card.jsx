155e50b687f589a0e09df92bb88f529cf4cfb0ae
JoaoVitorWinter <joaowinter@weg.net>
2024-02-21 20:00:55 -0300

    Botões de mais e menos do card funcionando

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,27 @@
+import { useEffect } from "react"
 import { useState } from "react"
 import Button from "./Button"
 
 const Card = ({name, quantity}) => {  
     const [currentQuantity, setCurrentQuantity] = useState(quantity)
+
+    useEffect(() => {
+      if (currentQuantity <= 0) {
+
+      }
+    }, [currentQuantity]);
+
+    const handleChange = (change) => {
+        setCurrentQuantity(currentQuantity + change);
+    }
+
     return (
     <div className="flex m-auto mt-6 rounded-xl bg-primary px-4 md:px-12 py-4 items-center justify-evenly gap-4 md:gap-20">
         <p className="">{name}</p>
         <div className="flex justify-around gap-2 items-center">
-        <button className="bg-black text-white w-6 rounded-sm">+</button>
-        <p className="">{quantity}</p>
-        <button className="bg-black text-white w-6 rounded-sm">-</button>
+        <button onClick={() => {handleChange(-1)}} className="bg-black text-white w-6 rounded-sm">-</button>
+        <p className="">{currentQuantity}</p>
+        <button onClick={() => {handleChange(1)}} className="bg-black text-white w-6 rounded-sm">+</button>
         </div>
         <Button text={"Excluir"}></Button>
     </div>
