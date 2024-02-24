c4282f556786c527483facd303630756d6ea743e
JoaoVitorWinter <joaovwinter3108@gmail.com>
2024-02-23 21:19:06 -0300

    Não permitindo outros caracteres

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,9 @@
-const Input = ({labelText, placeholder, name, onChange, type, width, value}) => {
+const Input = ({labelText, placeholder, name, onChange, onKeyPress, type, width, value}) => {
   return (
     <>
       <label className="font-base">
       {labelText}
-      <input onChange={onChange} placeholder={placeholder} value={value} type={type} name={name} className={`block ${width ? width : "w-auto"} mt-2 px-6 py-2 text-base rounded-lg`} />
+      <input onChange={onChange} onKeyPress={onKeyPress} placeholder={placeholder} value={value} type={type} name={name} className={`block ${width ? width : "w-auto"} mt-2 px-6 py-2 text-base rounded-lg`} />
       </label>
     </>
   );
